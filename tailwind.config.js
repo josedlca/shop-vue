881e9aed71bd9bb8aba5127c422012f3104d0518
josedlca <jose.dlcruz.ayala@gmail.com>
2020-05-21 17:39:02 -0500

    header contact complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     extend: {
       textColor: {
         'main-orange': 'rgb(255, 89, 18)',
+        'smooth-gray': 'rgb(52, 64, 75)',
+        'light-gray': 'rgb(244, 246, 248)'
+      },
+      spacing:{
+        '7':'1.75rem'
       }
     },
   },
