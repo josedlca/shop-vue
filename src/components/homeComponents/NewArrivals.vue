8072599a42880e962f8d0effdea3588e569c8f90
josedlca <jose.dlcruz.ayala@gmail.com>
2020-05-22 18:09:01 -0500

    Work props

---
+++ src/components/homeComponents/NewArrivals.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <TitleComponent :title="title"/>
+    </div>
+</template>
+
+<script>
+
+import TitleComponent from '../helpComponents/TitleComponent'
+export default {
+  components: {
+    TitleComponent
+  },
+  name: 'NewArrivals',
+  data () {
+    return {
+      title: 'some dummy text'
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <Contact/>
     <Navigation/>
     <Slider/>
+    <NewArrivals/>
   </div>
 </template>
 
@@ -11,13 +12,15 @@
 import Contact from '@/components/head/Contact.vue'
 import Navigation from '@/components/head/Navigation.vue'
 import Slider from '@/components/homeComponents/Slider.vue'
+import NewArrivals from '@/components/homeComponents/NewArrivals.vue'
 
 export default {
   name: 'Home',
   components: {
     Contact,
     Navigation,
-    Slider
+    Slider,
+    NewArrivals
   }
 }
 </script>
