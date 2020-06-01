1ef191995465ee847a77d9d47f9134a8d608ae2f
josedlca <jose.dlcruz.ayala@gmail.com>
2020-06-01 13:08:26 -0500

    products page starts

---
+++ src/views/Products.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class=" bg-light-gray">
+    <Contact/>
+    <Navigation/>
+    <NewsLetterComponent/>
+    <FooterComponent/>
+  </div>
+</template>
+
+<script>
+import Contact from '@/components/head/Contact.vue'
+import Navigation from '@/components/head/Navigation.vue'
+import NewsLetterComponent from '@/components/helpComponents/NewsLetterComponent.vue'
+import FooterComponent from '@/components/helpComponents/FooterComponent.vue'
+export default {
+  name: 'Products',
+  components: {
+    Contact,
+    Navigation,
+    NewsLetterComponent,
+    FooterComponent
+  }
+}
+</script>
